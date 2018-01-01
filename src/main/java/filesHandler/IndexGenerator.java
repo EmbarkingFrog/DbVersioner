f6069bd2ab4491cd7c483c30117fdf7685ed443b
Yuval Peled <peledyu@gmail.com>
2018-01-01 11:47:39 +0200

    - Refactored Version to be a sequence instead version semantics - Build now validates that versions are without duplicates and with no holes (ensures correct sequence)

--- src/main/java/filesHandler/IndexGenerator.java
+++ src/main/java/filesHandler/IndexGenerator.java
@@ -14,6 +14,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -21,7 +22,7 @@ import static filesHandler.FileReadUtils.parseVersion;
 
 public class IndexGenerator {
     private final static String rootFolder = "/sqlFiles";
-    private final static String baseBackupFile = rootFolder + "/1.0.0-backup-first_backup.sql";
+    private final static String baseBackupFile = rootFolder + "/0-backup-first_backup.sql";
 
     public static void main(String[] args) throws MojoFailureException, MojoExecutionException {
         IndexGenerator indexGenerator = new IndexGenerator();
@@ -51,56 +52,42 @@ public class IndexGenerator {
     }
 
     private void addSortedSchemasToIndex(JSONObject filesIndex, Path rootSqlFilesFolder) throws IOException {
-        class VersionedPath implements Comparable<VersionedPath> {
-            Path path;
-            Version version;
-
-            VersionedPath(Path p) {
-                this.path = p;
-                version = parseVersion(p);
-            }
-
-            @Override
-            public int compareTo(VersionedPath otherVersionedPath) {
-                return this.version.compareTo(otherVersionedPath.version);
-            }
-
-            @Override
-            public boolean equals(Object o) {
-                if (this == o) return true;
-                if (o == null || getClass() != o.getClass()) return false;
-
-                VersionedPath that = (VersionedPath) o;
-
-                return version.equals(that.version);
-            }
-
-            @Override
-            public int hashCode() {
-                return version.hashCode();
-            }
-
-            @Override
-            public String toString() {
-                return this.path.toString();
-            }
-        }
         List<Path> schemaPaths = getFilesInFolder(rootSqlFilesFolder.resolve("schemas"));
 
         TreeSet<VersionedPath> sortedSchemas = new TreeSet<>();
         for (Path schemaPath : schemaPaths) {
             VersionedPath schemaVersionedPath = new VersionedPath(schemaPath);
-                if (sortedSchemas.contains(schemaVersionedPath)) {
-                    throw new IllegalArgumentException(String.format("Can't add [%s], a schema script with this version already exists!",
-                            schemaVersionedPath ));
-                }
+            if (sortedSchemas.contains(schemaVersionedPath)) {
+                throw new IllegalArgumentException(String.format("Can't add [%s], a schema script with this version already exists!",
+                        schemaVersionedPath));
+            }
             sortedSchemas.add(schemaVersionedPath);
         }
+
+        validateSchemasHaveNoHole(sortedSchemas);
 
         JSONArray schemasJsonArray = new JSONArray();
         sortedSchemas.forEach(s -> schemasJsonArray.put(s.path));
 
         filesIndex.put("schemas", schemasJsonArray);
+    }
+
+    private void validateSchemasHaveNoHole(TreeSet<VersionedPath> sortedSchemas) {
+        Iterator<VersionedPath> iterator = sortedSchemas.iterator();
+        if (iterator.hasNext()) {
+            VersionedPath versionedPath = iterator.next();
+            VersionedPath nextVersionedPath;
+            while (iterator.hasNext()) {
+                nextVersionedPath = iterator.next();
+                if (!versionedPath.version.isConsecutive(nextVersionedPath.version)) {
+                    throw new IllegalArgumentException(String.format("There's a gap in the versions of the sql update" +
+                                    " script, between the files [%s] and [%s]",
+                            versionedPath.path.toString(), nextVersionedPath.path.toString()));
+                } else {
+                    versionedPath = nextVersionedPath;
+                }
+            }
+        }
     }
 
     private void writeIndexFile(JSONObject filesIndex, Path rootSqlFilesFolder) throws IOException {
@@ -132,5 +119,40 @@ public class IndexGenerator {
             }
         }
         return filesInFolder;
+    }
+
+    private class VersionedPath implements Comparable<VersionedPath> {
+        Path path;
+        Version version;
+
+        VersionedPath(Path p) {
+            this.path = p;
+            version = parseVersion(p);
+        }
+
+        @Override
+        public int compareTo(VersionedPath otherVersionedPath) {
+            return this.version.compareTo(otherVersionedPath.version);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            VersionedPath that = (VersionedPath) o;
+
+            return version.equals(that.version);
+        }
+
+        @Override
+        public int hashCode() {
+            return version.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return this.path.toString();
+        }
     }
 }
--- src/main/java/versions/Version.java
+++ src/main/java/versions/Version.java
@@ -1,57 +1,29 @@
 package versions;
 
 public class Version implements Comparable<Version> {
-    private final int major;
-    private final int minor;
-    private final int revision;
+    private final long versionNumber;
 
-    private final static int VERSION_DIVISIONS = 3;
-
-    public static Version LATEST_VERSION = new Version(Integer.MAX_VALUE + "." + Integer.MAX_VALUE + "." + Integer.MAX_VALUE);
+    public static Version LATEST_VERSION = new Version(Long.MAX_VALUE);
 
     public Version(String unparsedVersion) {
-        int[] parsedVersion = parseVersion(unparsedVersion);
-        this.major = parsedVersion[0];
-        this.minor = parsedVersion[1];
-        this.revision = parsedVersion[2];
+        if (unparsedVersion.equalsIgnoreCase("latest")) {
+            this.versionNumber = Long.MAX_VALUE;
+        } else {
+            this.versionNumber = Long.parseLong(unparsedVersion);
+        }
+    }
+
+    public Version(long version) {
+        this.versionNumber = version;
+    }
+
+    public boolean isConsecutive(Version version) {
+        return (Math.abs(this.versionNumber - version.versionNumber) == 1);
     }
 
     @Override
     public int compareTo(Version version) {
-        if (this.major > version.major) return 1;
-        else if (this.major < version.major) return -1;
-        else if (this.minor > version.minor) return 1;
-        else if (this.minor < version.minor) return -1;
-        else return Integer.compare(this.revision, version.revision);
-    }
-
-    private int[] parseVersion(String unparsedVersion) {
-        if (unparsedVersion.compareToIgnoreCase("latest") == 0){
-            int[] latestVersion = new int[3];
-            latestVersion[0] = Integer.MAX_VALUE;
-            latestVersion[1] = Integer.MAX_VALUE;
-            latestVersion[2] = Integer.MAX_VALUE;
-            return latestVersion;
-        }
-        String[] splitVersion = unparsedVersion.split("\\.");
-        if (splitVersion.length != VERSION_DIVISIONS) {
-            throw new IllegalArgumentException("Illegal Version numbers received! Must be in the form of [0.0.0], or [latest]. Received: "
-                    + unparsedVersion);
-        }
-        int[] parsedVersion = new int[VERSION_DIVISIONS];
-        for (int index = 0; index < splitVersion.length; index++) {
-            try {
-                int versionPart = Integer.parseInt(splitVersion[index]);
-                if (versionPart < 0) {
-                    throw new IllegalArgumentException("Version must contain positive numbers only. (Good example: 1.1.1, bad example: 1.-1.2." +
-                            "Received: " + versionPart);
-                }
-                parsedVersion[index] = versionPart;
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Version contains something that's not a number (" + splitVersion[index] + ")! Received: " + unparsedVersion, e);
-            }
-        }
-        return parsedVersion;
+        return ((Long) this.versionNumber).compareTo(version.versionNumber);
     }
 
     @Override
@@ -59,25 +31,20 @@ public class Version implements Comparable<Version> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        Version version = (Version) o;
+        Version otherVersion = (Version) o;
 
-        if (major != version.major) return false;
-        if (minor != version.minor) return false;
-        return revision == version.revision;
+        return this.versionNumber == otherVersion.versionNumber;
     }
 
     @Override
     public int hashCode() {
-        int result = major;
-        result = 31 * result + minor;
-        result = 31 * result + revision;
-        return result;
+        return (int) (versionNumber ^ (versionNumber >>> 32));
     }
 
     @Override
     public String toString() {
-        if (major == minor && minor == revision && revision == Integer.MAX_VALUE) {
+        if (versionNumber == Long.MAX_VALUE) {
             return "latest";
-        } else return major + "." + minor + "." + revision;
+        } else return "" + versionNumber;
     }
 }
